fe9d7bd3da926fede1ba20b2e8915154b6c42c23
Kethelyn Ellen <120947841+Kethyoliver@users.noreply.github.com>
2024-09-25 21:53:07 -0300

    componentes adicionados

--- src/telas/Cardapio/componentes/card.js
+++ src/telas/Cardapio/componentes/card.js
@@ -1,10 +1,53 @@
 import React, { useState } from 'react';
-import { TouchableOpacity, Text, Modal, Pressable, View, Image} from 'react-native';
+import { TouchableOpacity, Text, Modal, Pressable, View, Image, Alert} from 'react-native';
 import { Card } from 'react-native-paper';
 import styles from "../estilo";    
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
-export default function CardProd({ item: { id, nome, imagem, valor } }) {
-    const [modalVisible, setModalVisible] = useState(false);
+export default function CardProd({ item: {id, nome, imagem, valor } }) {
+    const [modalVisible, setModalVisible, open, setOpen] = useState(false);
+    
+    async function addListaDesejos(id, nome, imagem, valor){
+        
+        // Produto favoritado
+        const addProduto=[{
+            id: id,
+            nome: nome,
+            imagem: imagem,
+            valor: valor,
+        }];
+
+        //Verificar se a lista está vazia
+        const listaDesejosSalva = await AsyncStorage.getItem('ListaDesejos');
+        
+        if(listaDesejosSalva == null){
+            //Lista Vazia, insere o produto clicado
+            const ListaDesejosAtualizada = JSON.stringify(addProduto);
+
+            //Insere no AsyncStorage
+            await AsyncStorage.setItem('ListaDesejos', ListaDesejosAtualizada);
+
+            Alert.alert("O produto foi incluído com sucesso na Lista de Desejos!")
+            console.log("Adicionou produto");
+            console.log(ListaDesejosAtualizada);
+        } else {
+            // A lista já possui itens
+            const ListaDesejos = JSON.parse(listaDesejosSalva);
+
+            //insere mais um produto na lista
+            ListaDesejos.push({id: id, nome: nome, imagem: imagem});
+
+            //Converte o array para string
+            const ListaDesejosAtualizada = JSON.stringify(ListaDesejos);
+
+            //Insere no AsyncStorage
+            await AsyncStorage.setItem('ListaDesejos', ListaDesejosAtualizada);
+            Alert.alert("O produto foi incluído com sucesso na Lista de Desejos!")
+            console.log("Adicionou mais um produto na lista");
+            console.log(ListaDesejosAtualizada);
+        }
+    }
 
     return (
         <View style={styles.card}>
@@ -14,6 +57,9 @@ export default function CardProd({ item: { id, nome, imagem, valor } }) {
                     <Card.Content style={styles.container}>
                         <Text variant="titleLarge" style={styles.tituloCard}>{nome}</Text>
                         <Text variant="bodyMedium" style={styles.descricao}>{valor}</Text>
+                        <TouchableOpacity onPress={()=>addListaDesejos(id, nome, imagem, valor)}>
+                            <Ionicons name='heart' size={30} color="red"/>
+                        </TouchableOpacity>
                     </Card.Content>
                 </Card>
             </TouchableOpacity>
--- src/telas/Retrato/index.js
+++ src/telas/Retrato/index.js
@@ -1,11 +1,16 @@
 import { Camera, CameraType } from 'expo-camera/legacy';
-import { useState  } from 'react';
-import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { useState, useRef } from 'react';
+import { Button, StyleSheet, Text, TouchableOpacity, View, Image, TextInput } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { Card } from 'react-native-paper'
 
-export default function App() {
+
+export default function Index() {
   const [type, setType] = useState(CameraType.back);
   const [permission, requestPermission] = Camera.useCameraPermissions();
- 
+
+  const cameraRef = useRef(null);
+  const [capturedImage, setCapturedImage] = useState(null);
 
   if (!permission) {
 
@@ -16,7 +21,7 @@ export default function App() {
 
     return (
       <View style={styles.container}>
-        <Text style={{ textAlign: 'center' }}>We need your permission to show the camera</Text>
+        <Text style={{ textAlign: 'center' }}>Para fazer um lindo retrato, precisamos da sua permissão. Você pode nos permitir?</Text>
         <Button onPress={requestPermission} title="grant permission" />
       </View>
     );
@@ -26,15 +31,37 @@ export default function App() {
     setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
   }
 
+  async function tirarFoto() {
+    if (cameraRef) {
+      const photo = await cameraRef.current.takePictureAsync();
+      console.log("Foto foi tirada!", photo.uri);
+      setCapturedImage(photo.uri);
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Camera style={styles.camera} type={type}>
+      <Camera style={styles.camera} type={type} ref={cameraRef}>
         <View style={styles.buttonContainer}>
-          <TouchableOpacity style={styles.button} onPress={toggleCameraType}>
-            <Text style={styles.text}>Virar Camera</Text>
+          <TouchableOpacity style={styles.CameraVirar} onPress={toggleCameraType}>
+            <Ionicons name='reload' size={30} color='#fff'></Ionicons>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.CameraBotao} onPress={tirarFoto}>
+            <Ionicons name='camera' size={30} color='#fff'></Ionicons>
           </TouchableOpacity>
         </View>
       </Camera>
+      <Card.Content>
+        {
+          capturedImage &&
+          <View style={styles.fotoTirada}>
+            <Image source={{ uri: capturedImage }} style={styles.foto}></Image>
+            <TouchableOpacity  style={styles.botao} onPress={tirarFoto}>
+              <Text style={styles.textoBotao}>Nova foto</Text>
+            </TouchableOpacity>
+          </View>
+        }
+      </Card.Content>
     </View>
   );
 }
@@ -43,25 +70,55 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
+    backgroundColor: '#C69286',
   },
   camera: {
-    flex: 1,
-
+    width: '90%',
+    height: '45%',
+    alignSelf: 'center',
   },
   buttonContainer: {
     flex: 1,
     flexDirection: 'row',
     backgroundColor: 'transparent',
-    margin: 64,
   },
-  button: {
-    flex: 1,
-    alignSelf: 'flex-end',
-    alignItems: 'center',
+  CameraVirar: {
+    position: 'absolute',
+    bottom: 10,
+    left: 20,
+  },
+  CameraBotao: {
+    position: 'absolute',
+    bottom: 10,
+    right: '10%',
   },
   text: {
     fontSize: 24,
     fontWeight: 'bold',
     color: 'white',
+  },
+  fotoTirada: {
+    width: 200,
+    height: 200,
+    alignSelf: 'center',
+    borderRadius: '10',
+    marginBottom: 10,
+  },
+  foto: {
+    width: '100%',
+    height: '100%',
+    alignSelf: 'center',
+    borderRadius: '100',
+  },
+  botao: {
+    backgroundColor: '#C7830D',
+    padding: 10,
+    marginTop: 10,
+    alignItems: 'center',
+    borderRadius: 5,
+  },
+  textoBotao: {
+    color: '#fff',
+    fontSize: 16,
   },
 });
